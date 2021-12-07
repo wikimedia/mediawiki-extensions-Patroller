232d0c37ac7c38fac8173495f195975dec141155
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:24:38 +0100

    Emit deprecation warning from php entry point
    Change-Id: I9201c452e6e79fa3ca38014d7f3ffd51572a7d96

--- Patroller.php
+++ Patroller.php
@@ -12,6 +12,11 @@
 
 $dir = __DIR__;
 
+wfWarn(
+	'Deprecated PHP entry point used for Patroller extension. Please use wfLoadExtension instead, ' .
+	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+);
+
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Patroller' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
