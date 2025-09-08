6faba52113435786897edb3220ab78163359095b
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-09-08 04:39:36 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 48.0.0
    The following sniffs are failing and were disabled:
    * MediaWiki.Commenting.CommentBeforeClass.SpacingAfter
    * MediaWiki.Commenting.FunctionComment.MissingDocumentationPublic
    * MediaWiki.Commenting.LicenseComment.InvalidLicenseTag
    
    Change-Id: I99f733e6cd78786a48c471230e565ba225c7c5c6

--- includes/PatrollerHooks.php
+++ includes/PatrollerHooks.php
@@ -3,11 +3,11 @@
  * Patroller
  * Patroller MediaWiki main hooks
  *
- * @author: Rob Church <robchur@gmail.com>, Kris Blair (Developaws)
- * @copyright: 2006-2008 Rob Church, 2015-2017 Kris Blair
- * @license: GPL General Public Licence 2.0
- * @package: Patroller
- * @link: https://mediawiki.org/wiki/Extension:Patroller
+ * @author Rob Church <robchur@gmail.com>, Kris Blair (Developaws)
+ * @copyright 2006-2008 Rob Church, 2015-2017 Kris Blair
+ * @license GPL General Public Licence 2.0
+ * @package Patroller
+ * @link https://mediawiki.org/wiki/Extension:Patroller
  */
 
 class PatrollerHooks {
